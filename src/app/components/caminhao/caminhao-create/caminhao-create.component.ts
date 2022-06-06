8dbc6b6ba636e9cdefbb267e24a0727599c70223
Gerson Neto <gerson.neto@gmail.com>
2022-06-06 12:12:12 -0300

    AJuste da validação.

--- src/app/components/caminhao/caminhao-create/caminhao-create.component.ts
+++ src/app/components/caminhao/caminhao-create/caminhao-create.component.ts
@@ -24,15 +24,56 @@ export class CaminhaoCreateComponent implements OnInit {
   }
 
   createProduct(): void {
+  if (this.validacao){
     this.CaminhaoService.create(this.caminhao).subscribe(() => {
       this.CaminhaoService.showMessage('Produto Criado!')
       this.router.navigate(['/caminhao'])
     })
+  }
 
   }
 
   cancel(): void {
     this.router.navigate(['/caminhao'])
+  }
+  validacao():boolean{
+    var validar =true;
+    var mensagem="";
+    if (this.caminhao.anoModelo !=""|| this.caminhao.anoFabricacao!=""|| this.caminhao.modelo!="")
+    {
+      validar = false;
+      mensagem="Todos os campos são obrigatórios!";
+     }
+    if (this.caminhao.modelo!="FH" && this.caminhao.modelo != "FM")
+    {
+      validar=false;
+      if(mensagem.trim()!="")
+         mensagem+="<br>";
+      mensagem +="O modelo do caminhão deverá ser FM Ou FH";
+     }
+    if (this.caminhao.anoFabricacao.length!= 4 && isNaN(Number(this.caminhao.anoFabricacao))==false)
+    {
+     
+        validar=false;
+        if(mensagem.trim()!="")
+           mensagem+="<br>";
+         mensagem +="O ano de Fabricação não é valido";
+     
+           
+    }
+    if (this.caminhao.anoModelo.length!= 4 && isNaN(Number(this.caminhao.anoModelo))==false)
+    {      
+        validar=false;
+        if(mensagem.trim()!="")
+           mensagem+="<br>";
+         mensagem +="O ano do Modelo não é valido";     
+           
+    }
+    if (!validar)
+    {
+      this.CaminhaoService.showMessage(mensagem); 
+    }
+  return validar;
   }
 
 }
